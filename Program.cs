efcfef43b1dccf28ffa61c6446177e75768c5d29
LavanyaMPadma <lavanyamp1990@gmail.com>
2025-12-02 11:07:38 +0530

    Update Program.cs - dummy

--- Program.cs
+++ Program.cs
@@ -23,7 +23,6 @@ var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
-
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
@@ -44,5 +43,6 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
 
 
